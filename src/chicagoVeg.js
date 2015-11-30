8426a3fb24b27792317d5d6a8f0a648855f2bcc8
Phil <zoldello@gmail.com>
2015-11-29 18:56:50 -0600

    added more styling

--- src/chicagoVeg.js
+++ src/chicagoVeg.js
@@ -11,13 +11,13 @@ export class ChicagoVeg {
 
 		// need default router: https://github.com/aurelia/router/issues/62
 		config.map([
-			{ route: ['', 'home'], 'name':"home", moduleId: 'src/home/home', title: 'Home', nav: true },
-			{ route:  'mission', 'name':"mission", moduleId: 'src/mission/mission', title: 'Mission', nav: true },
-			{ route: 'news', 'name':"news", moduleId: 'src/news/news', title: 'News', nav: true }, 
-			{ route: 'resources', 'name':"resources", moduleId: 'src/resources/resources', title: 'Resources', nav: true },
-			{ route: 'donate', 'name':"donate", moduleId: 'src/donate/donate', title: 'Donate', nav: true }, 
-			{ route: 'contactus', 'name':"contactus", moduleId: 'src/contactus/contactus', title: 'Contact Us', nav: true },  
-			{ route: 'about', 'name':"about", moduleId: 'src/about/about', title: 'About', nav: true }  
+			{ route: ['', 'home'], 'name':"home", moduleId: './home/home', title: 'Home', nav: true },
+			{ route:  'mission', 'name':"mission", moduleId: './mission/mission', title: 'Mission', nav: true },
+			{ route: 'news', 'name':"news", moduleId: './news/news', title: 'News', nav: true }, 
+			{ route: 'resources', 'name':"resources", moduleId: './resources/resources', title: 'Resources', nav: true },
+			{ route: 'donate', 'name':"donate", moduleId: './donate/donate', title: 'Donate', nav: true }, 
+			{ route: 'contactus', 'name':"contactus", moduleId: './contactus/contactus', title: 'Contact Us', nav: true },  
+			{ route: 'about', 'name':"about", moduleId: './about/about', title: 'About', nav: true }  
 			]);
 	}
 }
