a78cff6e7859533df48278f55a364814621abb89
Phil <zoldello@gmail.com>
2015-12-24 15:20:12 -0600

    adjusted router list

--- src/chicagoVeg.js
+++ src/chicagoVeg.js
@@ -11,13 +11,13 @@ export class ChicagoVeg {
 
 		// need default router: https://github.com/aurelia/router/issues/62
 		config.map([
-			{ route: ['', 'home'], 'name':"home", moduleId: './home/home', title: 'Home', nav: true },
-			{ route:  'mission', 'name':"mission", moduleId: './mission/mission', title: 'Mission', nav: true },
-			{ route: 'news', 'name':"news", moduleId: './news/news', title: 'News', nav: true }, 
+			{ route: ['', 'home'], 'name':'home', moduleId: './home/home', title: 'Home', nav: true },
+			{ route: 'aboutus', 'name':'aboutus', moduleId: './aboutus/aboutus', title: 'About Us', nav: true },
+			{ route: 'programs', 'name':'programs', moduleId: './programs/programs', title: 'Programs', nav: true },
+			{ route: 'news', 'name':'news', moduleId: './news/news', title: 'News', nav: true }, 
 			{ route: 'resources', 'name':"resources", moduleId: './resources/resources', title: 'Resources', nav: true },
-			{ route: 'donate', 'name':"donate", moduleId: './donate/donate', title: 'Donate', nav: true }, 
-			{ route: 'contactus', 'name':"contactus", moduleId: './contactus/contactus', title: 'Contact Us', nav: true },  
-			{ route: 'about', 'name':"about", moduleId: './about/about', title: 'About', nav: true }  
+			{ route: 'restaurants', 'name':"restaurants", moduleId: './restaurants/restaurants', title: 'Restaurants', nav: true }, 
+			{ route: 'contactus', 'name':"contactus", moduleId: './contactus/contactus', title: 'Contact Us', nav: true } 			 
 			]);
 	}
 }
