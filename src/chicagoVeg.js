752cff525e8ca9cb48a94f4954cc2247f90d78ef
Phil <zoldello@gmail.com>
2015-12-26 20:19:01 -0600

    fixed issues with markers-filtering

--- src/chicagoVeg.js
+++ src/chicagoVeg.js
@@ -7,11 +7,11 @@ export class ChicagoVeg {
 
 	configureRouter(config, router) {
 		config.title = 'ChicagoVeg';
-		this.router = router;
-
+		
+		
 		// need default router: https://github.com/aurelia/router/issues/62
 		config.map([
-			{ route: ['', 'home'], 'name':'home', moduleId: './home/home', title: 'Home', nav: true },
+			{ route: ['', 'home'], 'name':'home', moduleId: './home/home', title: 'Home', nav: true, isDropDownable: true },
 			{ route: 'aboutus', 'name':'aboutus', moduleId: './aboutus/aboutus', title: 'About Us', nav: true },
 			{ route: 'programs', 'name':'programs', moduleId: './programs/programs', title: 'Programs', nav: true },
 			{ route: 'news', 'name':'news', moduleId: './news/news', title: 'News', nav: true }, 
@@ -19,5 +19,7 @@ export class ChicagoVeg {
 			{ route: 'restaurants', 'name':"restaurants", moduleId: './restaurants/restaurants', title: 'Restaurants', nav: true }, 
 			{ route: 'contactus', 'name':"contactus", moduleId: './contactus/contactus', title: 'Contact Us', nav: true } 			 
 			]);
+
+		this.router = router; //save router reference
 	}
 }
--- src/nav-bar/nav-bar.js
+++ src/nav-bar/nav-bar.js
@@ -3,6 +3,8 @@ import $ from 'github:components/jquery@2.1.4';
 import bootstrap from 'twbs/bootstrap';
 import {bindable} from 'aurelia-framework'
 
+//TODO: Figure out a nicer way to add a drop down
 export class NavBar {
 	@bindable router = null;
 }
+
