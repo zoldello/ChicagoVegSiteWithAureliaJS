0e03c04fd5e2693e50d897e81aac18480656cafe
Phil <zoldello@gmail.com>
2016-01-02 14:12:43 -0600

    updatd css

--- src/chicagoVeg.js
+++ src/chicagoVeg.js
@@ -26,110 +26,156 @@ export class ChicagoVeg {
 			title: "About Us",
 			nav: true, 
 			settings:{
-      			subMenu:[
-        			{	href:'#/history', 
-        				title:'History',
-        				route: "history",
-						"name": "history",
-						moduleId: "./history/history",
-						title: "History",
-						nav: true, 
-        			},
-        			{
-        				href:'#/mission', 
-        				title:'Mision',
-        				route: "mission",
-						"name": "mission",
-						moduleId: "./mission/mission",
-						title: "Mision", 
-						nav: true, 
-					}
-      			]
-    		}
-		},
+				subMenu:[
+				{	href:'#/history', 
+				title:'History',
+				route: "history",
+				"name": "history",
+				moduleId: "./history/history",
+				title: "History",
+				nav: true, 
+			},
+			{
+				href:'#/mission', 
+				title:'Mision',
+				route: "mission",
+				"name": "mission",
+				moduleId: "./mission/mission",
+				title: "Mision", 
+				nav: true, 
+			}
+			]
+		}
+	},
 
-		{
-			route: "programs",
-			"name": "programs",
-			moduleId: "./programs/programs",
-			title: "Programs",
-			nav: true, 
-			settings:{
-      			subMenu:[
-        			{
-        				href:'http://www.meetup.com/ChicagoVegOutreach/', 
-        				title:'Outreach',
-        				openNewPage: true
-        			},
-        			{
-        				href:'http://www.meetup.com/ChicagoVegCooking/', 
-        				title:'Cooking',
-        				openNewPage: true},
-        			{
-        				href:'http://www.google.com', 
-        				title:'Singles',
-        				openNewPage: true
-        			},
-        			{
-        				href:' http://www.meetup.com/ChicagoVegKids/', 
-        				title:'Family/Kids',
-        				openNewPage: true
-        			}
-      			]
-    		}
-		},
-		{
-			route: "news",
-			"name": "news",
-			moduleId: "./news/news",
-			title: "News",
-			nav: true
-		}, {
-			route: "resources",
-			"name": "resources",
-			moduleId: "./resources/resources",
-			title: "Resources",
-			nav: true
-		}, {
-			route: "restaurants",
-			"name": "restaurants",
-			moduleId: "./restaurants/restaurants",
-			title: "Restaurants",
-			nav: true, 
-			settings:{
-      			subMenu:[
-        			{ 
-        				href:'http://restaurants.chicagoveg.com', 
-        				title:'Finder' ,
-        				openNewPage: true
-        			},
-        			{ 
-        				href:'http://www.meetup.com/ChicagoVeg/pages/ChicagoVeg_Choice_2015/', 
-        				title:'ChicagoVeg Choice',
-        				openNewPage: true}
-      			]
-    		}
+	{
+		route: "programs",
+		"name": "programs",
+		moduleId: "./programs/programs",
+		title: "Programs",
+		nav: true, 
+		settings:{
+			subMenu:[
+			{
+				href:'#/outreach', 
+				title:'Outreach',
+				openNewPage: false,
+				route:"outreach",
+				"name": "outreach",
+				moduleId: "./outreach/outreach",
+				nav: true
+			},
+			{
+				href:'#/cooking', 
+				title:'Cooking',
+				route:"cooking",
+				"name": "cooking",
+				moduleId: "./cooking/cooking",
+				openNewPage: false,
+				nav: true
+			},
+			{
+				
+				href:'#/singles', 
+				title:'Singles',
+				openNewPage: false,
+				route:"singles",
+				"name": "singles",
+				moduleId: "./singles/singles",
+				nav: true
+			},
+			{
+				href:' #/family-kids', 
+				title:'Family/Kids',
+				openNewPage: false, 
+				route:"family-kids",
+				"name": "family-kids",
+				moduleId:"./family-kids/family-kids",
+				nav: true
+			}
+			]
+		}
+	},
+	{
+		route: "news",
+		"name": "news",
+		moduleId: "./news/news",
+		title: "News",
+		nav: true
+	}, {
+		route: "resources",
+		"name": "resources",
+		moduleId: "./resources/resources",
+		title: "Resources",
+		nav: true
+	}, {
+		route: "restaurants",
+		"name": "restaurants",
+		moduleId: "./restaurants/restaurants",
+		title: "Restaurants",
+		nav: true, 
+		settings:{
+			subMenu:[
+			{ 
+				href:'http://restaurants.chicagoveg.com', 
+				title:'Restaurant Finder' ,
+				openNewPage: true
+			},
+			{ 
+				href:'http://www.meetup.com/ChicagoVeg/pages/ChicagoVeg_Choice_2015/', 
+				title:'ChicagoVeg Choice',
+				openNewPage: true}
+				]
+			}
 		}, {
 			route: "contactus",
 			"name": "contactus",
 			moduleId: "./contactus/contactus",
 			title: "Contact Us",
 			nav: true
-			},
-			{
+		},
+		{
 			route: "history",
 			"name": "history",
 			moduleId: "./history/history",
 			title: "History",
 			nav: false
-			},
-			{
+		},
+		{
 			route: "mission",
 			"name": "mission",
 			moduleId: "./mission/mission",
 			title: "Mision",
 			nav: false
-		}		 		 		 		 
+		},
+		{
+			route: "outreach",
+			"name": "outreach",
+			moduleId: "./outreach/outreach",
+			title: "Outreach",
+			nav: false
+		}, 		 		 
+		{
+			route: "cooking",
+			"name": "cooking",
+			moduleId: "./cooking/cooking",
+			title: "Cooking",
+			nav: false
+		}, 
+		{
+			route: "singles",
+			"name": "singles",
+			moduleId: "./singles/singles",
+			title: "Singles",
+			nav: false
+		},
+		{
+			route: "family-kids",
+			"name": "family-kids",
+			moduleId: "./family-kids/family-kids",
+			title: "Family/kids",
+			nav: false
+		}		 			 			 		 			 		 
 		]);
 
 		this.router = router; //save router reference
