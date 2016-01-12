3af454fbf44426c4272f215f6856766c8e8293f5
Phil <zoldello@gmail.com>
2016-01-11 20:31:07 -0600

    added Rebecka's changes

--- src/contactus/contactus.js
+++ src/contactus/contactus.js
@@ -1,3 +1,16 @@
 export class ContactUs {
-	
+	created() {
+		let this.grecaptcha.render('html_element', {
+    		'sitekey' : '6LeFZxITAAAAAKt6qHypxwPLcvY78lA0IF4uXyvj'
+  		});
+	}
+
+	formSubmit() {
+		if(this.grecaptcha.getResponse() == "") {
+			e.preventDefault();
+			alert("You can't proceed!");
+		} else {
+			alert("Thank you");
+		}
+	}	
 }
